7950c8cb6f01f4e9197ed84f36ec31e08c407ca4
Kaan Kaya <kaankaya.0707@gmail.com>
2024-08-07 02:10:47 +0300

    Add project files.

---
+++ OOP7/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP7
+{
+    //abstract class tanımı
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Departmant { get; set; }
+        public string Job { get; set; }
+        //abstract method tanımı
+        public abstract void Duty();
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Ad : {Name} Soyad : {Surname} Departman: {Departmant} ");
+        }
+    }
+    //Person dan kalıtım alıyor
+    public class Developer : Person
+    {
+        
+
+        public Developer(string name,string surname,string departmant,string job) 
+        { 
+            Name = name;
+            Surname = surname;
+            Departmant = departmant;
+            Job = job;
+        }
+        
+        public override void Duty()
+        {
+            
+            Console.WriteLine($"{Job} olarak Çalışıyorum");
+        }
+    }
+}
---
+++ OOP7/Program.cs
@@ -0,0 +1,12 @@
+﻿using OOP7;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person develop = new Developer("Hüseyin","Sarık","Backend","Software Developer");
+        develop.DisplayInfo();
+        develop.Duty();
+
+    }
+}
